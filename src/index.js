5b998c0d18ddcf1d9686f7c16ce22bdb15806f1f
Jonathan de Jesus Martinez Sanchez <20231417@itla.edu.do>
2025-08-21 15:25:08 -0400

    Subida a Produccion

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hola Mundo desde DevOps CI/CD!'));
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server listening on ${port}`));
-
 module.exports = app;
+
+// Si se ejecuta directamente (node src/index.js), entonces escuchamos:
+if (require.main === module) {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`Server listening on ${port}`));
+}
